a7ef423b720249827614be2de2a54975901a5245
Connor Myers <connor.myers21@outlook.com>
2016-03-09 18:02:02 -0800

    Added a lot of functionality to the site

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,29 +1,15 @@
 $(document).ready(function() {
 
   var ref = new Firebase("https://bg-outfitters.firebaseio.com");
-  // ref.createUser({
-  //   email: "bobtony@firebase.com",
-  //   password: "correcthorsebatterystaple"
-  // }, function(error, userData) {
-  //   if (error) {
-  //     switch (error.code) {
-  //       case "EMAIL_TAKEN":
-  //         console.log("The new user account cannot be created because the email is already in use.");
-  //         break;
-  //       case "INVALID_EMAIL":
-  //         console.log("The specified email is not a valid email.");
-  //         break;
-  //       default:
-  //         console.log("Error creating user:", error);
-  //     }
-  //   } else {
-  //     console.log("Successfully created user account with uid:", userData.uid);
-  //   }
-  // });
+  var huntingRef = ref.child("huntingTypes");
 
   function AdminViewModel() {
 
     var self = this;
+
+    self.selectedAnimalType = ko.observable("");
+    self.animalTypeAnimals = ko.observableArray("");
+    self.huntTypes = ko.observableArray("");
 
     self.huntDatesContainerClicked = function() {
       $('#hunt-calendar-section').fadeIn();
@@ -43,14 +29,48 @@ $(document).ready(function() {
     };
 
     self.huntPriceContainerClicked = function() {
+      self.huntTypes([]);
+      console.log(self.huntTypes());
+      firebaseFuncs.ref.child("/huntingTypes").once("value", function(snapshot) {
+        var huntingTypes = snapshot.val();
+        $.each(huntingTypes, function(index,value) {
+          self.huntTypes.push(value);
+        });
+        console.log(self.huntTypes());
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
       $('#hunt-price-section').fadeIn();
+      // huntingRef.set(self.animals);
     };
 
-  };
+    self.editButtonClicked= function(huntType) {
+      self.animalTypeAnimals(huntType.animal);
+      self.selectedAnimalType(huntType.animalType);
+      $('.hunt-prices-container').fadeOut(500,function() {
+          $('.hunt-clicked-edit-container').fadeIn();
+      });
+
+    };
+
+    self.exitEditContatinerClicked = function() {
+      $('#hunt-price-section').css('display', 'block !important');
+      $('.hunt-clicked-edit-container').fadeOut(500, function() {
+          $('.hunt-prices-container').fadeIn();
+      });
+    };
+
+    self.saveHuntPriceUpdate = function(data) {
+      console.log(self.huntTypes());
+      huntingRef.update(self.huntTypes());
+    };
+
+};
 
 
   var adminObjectVm = new AdminViewModel();
   ko.applyBindings(adminObjectVm,$("#admin")[0]);
+  ko.applyBindings(adminObjectVm,$("#hunt-price-section")[0]);
 
   window.onload = function() {
     var authData = ref.getAuth();
--- public/js/hunting.js
+++ public/js/hunting.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+  var ref = new Firebase("https://bg-outfitters.firebaseio.com");
+  var huntingRef = ref.child("huntingTypes");
+
   var HuntingViewModel = function() {
 
     var self = this;
@@ -7,17 +11,24 @@ $(document).ready(function() {
     self.locations = ko.observable("");
     self.hunt_description = ko.observable("");
     self.locationFrameSRC = ko.observable("");
+    self.animalSelected = ko.observableArray("");
+    self.locationsAnimals = ko.observableArray("");
 
     self.huntItemClicked = function(huntingType) {
-      self.hunt_type(huntingType.animalName);
-      self.hunt_description(huntingType.longDescription);
+      console.log(huntingType);
+      self.hunt_type(huntingType.animalType);
+      self.hunt_description(huntingType.description);
+      self.animalSelected(huntingType.animal);
+      console.log(self.animalSelected());
       $('.hunt-type-popup').fadeIn();
       $('body').css('overflow','hidden');
     }
 
     self.locationClicked = function(huntingType) {
-      self.hunt_type(huntingType.animalName);
-      self.locations(huntingType.locations);
+      self.locationsAnimals(huntingType.animal);
+      console.log(self.locationsAnimals());
+      //self.hunt_type(huntingType.animalName);
+      //self.locations(huntingType.locations);
       $('.hunt-location-popup').fadeIn();
       $('body').css('overflow','hidden');
     }
@@ -27,112 +38,18 @@ $(document).ready(function() {
       $('.frame-container').fadeIn();
     }
 
-    self.huntingTypes = [
-      {
-        animalName: 'Deer',
-        timeOfYear: 'Spring',
-        imageURL: '/static/img/deer-white.png',
-        longDescription: 'Lorem ipsum dolor sit amet, duo an esse erroribus rationibus, ea augue ignota tibique sea. Ex legendos intellegam sed, eu erat appareat ocurreret vix, essent scripserit cu qui. Modo quot quaestio cum te. Cum purto erant scriptorem at.Vis et suavitate moderatius reprimique. Nec ad facilisis eloquentiam vituperatoribus. Saperet iracundia comprehensam duo at, ex laudem suscipit persecuti usu. At qui oratio oporteat hendrerit, ex velit causae praesent est, in qui postulant dignissim. Sumo veritus mel et, aeque sonet quaerendum nec ad.',
-        locations: [
-          {
-            city: 'Fresno',
-            state: 'California',
-            zipcode: '93726',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204504.2867585118!2d-119.93499130567622!3d36.78545091603865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945de1549e4e9d%3A0x7b12406449a3b811!2sFresno%2C+CA!5e0!3m2!1sen!2sus!4v1455751761765'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93619',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93611',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          }
-        ]
-      },
-      {
-        animalName: 'Pig',
-        timeOfYear: 'Summer',
-        imageURL: '/static/img/pig-white.png',
-        longDescription: 'Lorem ipsum dolor sit amet, duo an esse erroribus rationibus, ea augue ignota tibique sea. Ex legendos intellegam sed, eu erat appareat ocurreret vix, essent scripserit cu qui. Modo quot quaestio cum te. Cum purto erant scriptorem at.Vis et suavitate moderatius reprimique. Nec ad facilisis eloquentiam vituperatoribus. Saperet iracundia comprehensam duo at, ex laudem suscipit persecuti usu. At qui oratio oporteat hendrerit, ex velit causae praesent est, in qui postulant dignissim. Sumo veritus mel et, aeque sonet quaerendum nec ad.',
-        locations: [
-          {
-            city: 'Fresno',
-            state: 'California',
-            zipcode: '93726',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204504.2867585118!2d-119.93499130567622!3d36.78545091603865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945de1549e4e9d%3A0x7b12406449a3b811!2sFresno%2C+CA!5e0!3m2!1sen!2sus!4v1455751761765'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93619',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93611',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          }
-        ]
-      },
-      {
-        animalName: 'Waterfowl',
-        timeOfYear: 'Fall',
-        imageURL: '/static/img/duck-white.png',
-        longDescription: 'Lorem ipsum dolor sit amet, duo an esse erroribus rationibus, ea augue ignota tibique sea. Ex legendos intellegam sed, eu erat appareat ocurreret vix, essent scripserit cu qui. Modo quot quaestio cum te. Cum purto erant scriptorem at.Vis et suavitate moderatius reprimique. Nec ad facilisis eloquentiam vituperatoribus. Saperet iracundia comprehensam duo at, ex laudem suscipit persecuti usu. At qui oratio oporteat hendrerit, ex velit causae praesent est, in qui postulant dignissim. Sumo veritus mel et, aeque sonet quaerendum nec ad.',
-        locations: [
-          {
-            city: 'Fresno',
-            state: 'California',
-            zipcode: '93726',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204504.2867585118!2d-119.93499130567622!3d36.78545091603865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945de1549e4e9d%3A0x7b12406449a3b811!2sFresno%2C+CA!5e0!3m2!1sen!2sus!4v1455751761765'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93619',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93611',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          }
-        ]
-      },
-      {
-        animalName: 'Bear',
-        timeOfYear: 'Winter',
-        imageURL: '/static/img/bear-white.png',
-        longDescription: 'Lorem ipsum dolor sit amet, duo an esse erroribus rationibus, ea augue ignota tibique sea. Ex legendos intellegam sed, eu erat appareat ocurreret vix, essent scripserit cu qui. Modo quot quaestio cum te. Cum purto erant scriptorem at.Vis et suavitate moderatius reprimique. Nec ad facilisis eloquentiam vituperatoribus. Saperet iracundia comprehensam duo at, ex laudem suscipit persecuti usu. At qui oratio oporteat hendrerit, ex velit causae praesent est, in qui postulant dignissim. Sumo veritus mel et, aeque sonet quaerendum nec ad.',
-        locations: [
-          {
-            city: 'Fresno',
-            state: 'California',
-            zipcode: '93726',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204504.2867585118!2d-119.93499130567622!3d36.78545091603865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945de1549e4e9d%3A0x7b12406449a3b811!2sFresno%2C+CA!5e0!3m2!1sen!2sus!4v1455751761765'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93619',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          },
-          {
-            city: 'Clovis',
-            state: 'California',
-            zipcode: '93611',
-            frame: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3194.5489719692314!2d-119.73515304962442!3d36.80536297984909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945c50ce9ced1b%3A0xb33f6498269cbbbb!2s4819+N+Winery+Cir%2C+Fresno%2C+CA+93726!5e0!3m2!1sen!2sus!4v1455752860689'
-          }
-        ]
-      }
-    ]
+    self.huntTypes = ko.observableArray("");
+
+    firebaseFuncs.ref.child("/huntingTypes").once("value", function(snapshot) {
+      var huntingTypes = snapshot.val();
+      $.each(huntingTypes, function(index,value) {
+        self.huntTypes.push(value);
+      });
+      console.log(self.huntTypes());
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+
   };
 
   var huntingObjectVm = new HuntingViewModel();

26ebaeabdc75229c33b2a76b60985deb70461582
timjongun <timalanfarrow@gmail.com>
2016-07-11 10:43:39 -0700

    break point

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,36 +1,10 @@
 (function($) {
 
-  function footerViewModel() {
+  function copyrightViewModel() {
 
       var self = this;
 
       self.currentDate = ko.observable("");
-
-      self.facebookClick = function() {
-        location.href = 'https://www.facebook.com/Passenger-Mobile-867565216683366/';
-      };
-
-      self.instagramClick = function() {
-        location.href = 'https://www.instagram.com/passengermobile/';
-      };
-
-      self.youtubeClick = function() {
-        location.href = 'https://www.youtube.com/channel/UCeF6PanOUEmKjTaFqnGeLow';
-      };
-
-      self.twitterClick = function() {
-        location.href = 'https://twitter.com/passengermobile';
-      };
-
-      self.snapchatClick = function() {
-        location.href = 'https://www.snapchat.com/add/passengermobile';
-      };
-
-      self.backToTopFooter = function() {
-        $('html, body').animate({ scrollTop: 0 }, 'slow', function () {
-
-        });
-      };
 
       var date = new Date();
       var year = date.getFullYear();
@@ -40,8 +14,8 @@
   };
 
 
-  var footerObjectVm = new footerViewModel();
-  ko.applyBindings(footerObjectVm,$("#footer")[0]);
+  var copyrightObjectVm = new copyrightViewModel();
+  ko.applyBindings(copyrightObjectVm,$(".copy-right-footer")[0]);
 
 
 })(jQuery); // End of use strict

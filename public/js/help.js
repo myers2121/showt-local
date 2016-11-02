ec814fb2834d3020a73a1224b6af6169497ac2cc
myers2121 <connor.myers21@gmail.com>
2016-11-02 14:23:22 -0700

    Fixed help questions
    Fixed the help questions on the website.

--- public/js/help.js
+++ public/js/help.js
@@ -12,7 +12,7 @@
         questionType: 'General information',
         questions: ["What is Passenger?", "How many cities are you in?", "Do you have any online partners?"],
         answers: ["Passenger is a mobile phone application that rewards its users for not texting and driving and giving them discounts at their favorite local businesses.",
-         "We are currently only in Fresno, CA. However, we are rapidly expanding to cities and states nearby.",
+         "We are currently in Fresno, Clovis, and Kingsburg. However, we are going to be expanding to Sacramento and Visalia in California soon. If you want to become a rep in your city contact us!",
           "At the moment we are not partnered with any online store. But, we are actively pursuing and in constant negotiation with online retailers."]
       },
       {
@@ -22,25 +22,23 @@
         "As a business, how do we sign up to have an account and offer discounts on Passenger?"],
         answers: ["As of right now the ability to see your account on the Passenger website is not available. However, we are actively working on building a dashboard that will allow our users to see their profile online.",
         "Again, just like the personal accounts, we do not currently have a dashboard for our partners either. But, our users and our partners come first. This is why we are currently building out a dashboard for our partners to see their account information whenever they want it.",
-        "In the coming months we are going to be releasing our partners dashboard which will allow them to sign up for an account and see all of the information that they could ever want for their account. But, for now, just by simply emailing us we will respond to you in less than 24 hours with a phone call to get going with creating you an account on Passenger."]
+        "To get in contact with us click the merchants link in the navigation bar at the top of the webpage and fill out the contact form. One of our sales representatives will be in contact with you within 24 hours."]
       },
       {
         questionType: 'Discounts',
         questions: ["What type of discounts do you offer?",
         "Where can we find the locations for your discounts?",
-        "When are you going to be in another city?"],
-        answers: ["Currently, most of the discounts that we offer are at local businesses around the city of Fresno that are typically small or family owned businesses. However, we are working every single day to expand our discounts to online and larger chain stores.",
-        "To see a list of the businesses that we currently have on the app you are going to log on to the Passenger app. When you open the app, click on the rewards section of the home screen. This will give you a look at all of the discounts that we currently offer in your city.",
-        "We are working every single day to expand our discounts reach to other cities nearby Fresno, California. Eventually, we would like to be in every single city in the United States. Keep coming back to the Passenger website to see if we have arrived in your city!"]
+        "When are you going to be coming to my city?"],
+        answers: ["Currently, most of the discounts that we offer are at local businesses around the cities that we are in. However, we are working every single day to expand our discounts to online and larger chain stores in your city. If you have any businesses to recommend feel free to contact us!",
+        "You can see the locations that we have both in on the Passenger Mobile website & on the app itself.",
+        "We are working every single day to expand our discounts reach to other cities. If you want the app in your city then please let us know by filling out our contact form. Keep coming back to the Passenger website to see if we have arrived in your city!"]
       },
       {
         questionType: 'Mobile Application',
         questions: ["Are you guys going to be putting the Passenger app on Android?",
-        "Does the app cost anything?",
-        "I can't download the app on my iPhone 5"],
-        answers: ["As of right now the app is only on iOS but we are currently building the Android version of the app to bring our friends on Android the wonderful experience of Passenger."
-        , "No. The app is free for our users, forever. We believe that helping you live a less distracted life should not cost you a dime.",
-        "Due to the needs of the Passenger app, the iPhone 5 is not capable of running the app. We want to make sure that all of our users have a delighted experience on the app and didn't want to compromise anything."]
+        "Does the app cost anything?"],
+        answers: ["As of right now the app is only on iOS but we are currently building the Android version of the app to bring our friends on Android the wonderful experience of Passenger. We are expecting a January 1st launch date."
+        , "No. The app is free for our users, forever. We believe that helping you live a less distracted life should not cost you a dime."]
       }
     ]);
 

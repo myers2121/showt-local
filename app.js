58ab6c65638627a7acf390beeeb3cc2efcb1e65b
myers2121 <connor.myers21@gmail.com>
2017-01-09 21:08:51 -0800

    Chenanged environment

--- app.js
+++ app.js
@@ -29,8 +29,6 @@ app.get('*', function(req, res){
   res.render('404');
 });
 
-var port = process.env.PORT || 3000;
-
-app.listen( port, function() {
+app.listen( process.env.PORT || 3000, function() {
 	console.log("The frontend server is running on port " + port + "!");
 });
